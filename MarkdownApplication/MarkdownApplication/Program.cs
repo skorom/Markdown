339287c00737c9b537d25254e66e694d673440ea
Péterfy Bori <onodybence@gmail.com>
2018-10-16 18:21:11 +0200

    JSON convert próbálkozás

--- MarkdownApplication/MarkdownApplication/Program.cs
+++ MarkdownApplication/MarkdownApplication/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
 
 namespace MarkdownApplication
 {
@@ -10,6 +13,12 @@ namespace MarkdownApplication
     {
         static void Main(string[] args)
         {
+            Read(Console.ReadLine());
+        }
+        public static void Read(string filepath)
+        {
+            string file = File.ReadAllText(filepath);
+            List<Text> Text = JsonConvert.DeserializeObject<Text>(file);
         }
     }
 }
