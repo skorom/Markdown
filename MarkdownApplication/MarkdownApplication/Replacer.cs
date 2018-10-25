0a744c129241dea5cf98be3dde5bc0c84a2ef79f
Pogácsa Sajtos <gernyimark@outlook.hu>
2018-10-25 16:58:42 +0200

    Replacer init

---
+++ MarkdownApplication/MarkdownApplication/Replacer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MarkdownApplication
+{
+    class Replacer
+    {
+	    public string replace(string input, string wordToFind, string replacement) {
+            string pattern = String.Format(@"\b{0}\b", wordToFind);
+            return Regex.Replace(input, pattern, replacement);
+	    }
+    }
+}
